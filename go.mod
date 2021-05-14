56b5db174d68b8370026851fcb3ba39fc53701a7
kyokuheishin <kyokuheishin@gmail.com>
2021-05-14 11:54:19 +0900

    Add the function of checking live status at regular intervals

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module bliverecorderg
 
 go 1.13
 
-require github.com/bitly/go-simplejson v0.5.0
+require (
+	github.com/bitly/go-simplejson v0.5.0
+	github.com/robfig/cron/v3 v3.0.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -12,30 +13,21 @@ import (
 	"time"
 
 	"github.com/bitly/go-simplejson"
+	"github.com/robfig/cron/v3"
 )
 
 var roomId = flag.Int("roomid", -1, "The id of the room which you want to record.")
 
+var roomChecklist = make(chan int)
+
 const bilibiliRoomApiUri = "https://api.live.bilibili.com/room/v1/Room/room_init"
 const bilibiliLiveRealStreamUri = "https://api.live.bilibili.com/xlive/web-room/v1/playUrl/playUrl"
 
-func main() {
-	flag.Parse()
-
-	if *roomId == -1 {
-		fmt.Println("Please input a valid room id.")
-		return
-	}
-	// fmt.Println("room id.")
-
+func checkLiveStatus(roomId int) (realRoomId int, liveStatus bool, err error) {
 	request, err := http.NewRequest("GET", bilibiliRoomApiUri, nil)
 
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	params := request.URL.Query()
-	params.Add("id", strconv.Itoa(*roomId))
+	params.Add("id", strconv.Itoa(roomId))
 	request.URL.RawQuery = params.Encode()
 
 	timeout := time.Duration(5 * time.Second)
@@ -44,49 +36,49 @@ func main() {
 	}
 
 	response, err := client.Do(request)
+
 	if err != nil {
-		log.Fatal(err)
+		return -1, false, err
 	}
 
 	defer response.Body.Close()
 
 	body, err := ioutil.ReadAll(response.Body)
 	fmt.Println(string(body))
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	respJson, err := simplejson.NewJson(body)
-
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	code := respJson.Get("code").MustInt()
 	fmt.Println(code)
 	if code != 0 {
-		fmt.Printf("Unknown error. Maybe there are some issues of Bilibili's server")
-		return
+
+		return -1, false, errors.New("Unknown error. Maybe there are some issues of Bilibili's server")
+	}
+	fmt.Println(string(body))
+	liveStatus = respJson.GetPath("data", "live_status").MustInt() != 0
+
+	realRoomId = respJson.GetPath("data", "room_id").MustInt()
+
+	return realRoomId, liveStatus, err
+}
+
+func liveVideoDownloader(realRoomId int) {
+
+	timeout := time.Duration(5 * time.Second)
+	client := &http.Client{
+		Timeout: timeout,
 	}
 
-	liveStatus := respJson.GetPath("data", "live_status").MustInt()
-	fmt.Println(liveStatus)
-	if liveStatus == 0 {
-		fmt.Printf("Not living.")
-		return
-	}
-
-	realRoomId := respJson.GetPath("data", "room_id").MustInt()
-	fmt.Println(realRoomId)
 	streamUrlRequest, err := http.NewRequest("GET", bilibiliLiveRealStreamUri, nil)
 
 	if err != nil {
-		log.Fatal(err)
+		log.Default().Printf(err.Error())
+		return
 	}
 
 	realRoomIdStr := strconv.Itoa(realRoomId)
 
-	params = streamUrlRequest.URL.Query()
+	params := streamUrlRequest.URL.Query()
 	params.Add("cid", realRoomIdStr)
 	params.Add("qn", strconv.Itoa(10000))
 	params.Add("platform", "web")
@@ -97,21 +89,24 @@ func main() {
 
 	streamUrlResponse, err := client.Do(streamUrlRequest)
 	if err != nil {
-		log.Fatal(err)
+		log.Default().Printf(err.Error())
+		return
 	}
 
 	defer streamUrlResponse.Body.Close()
 
 	streamUrlBody, err := ioutil.ReadAll(streamUrlResponse.Body)
-	// fmt.Println(string(streamUrlBody))
+
 	if err != nil {
-		log.Fatal(err)
+		log.Default().Printf(err.Error())
+		return
 	}
 
 	streamUrlJson, err := simplejson.NewJson(streamUrlBody)
 
 	if err != nil {
-		log.Fatal(err)
+		log.Default().Printf(err.Error())
+		return
 	}
 
 	realStreamUrl := streamUrlJson.GetPath("data", "durl").GetIndex(0).Get("url").MustString()
@@ -121,29 +116,71 @@ func main() {
 	err = os.MkdirAll(realRoomIdStr, os.ModePerm)
 
 	if err != nil {
-		log.Fatal(err)
+		log.Default().Printf(err.Error())
+		return
 	}
 	nowTime := time.Now()
 	formattedNowTimeStr := nowTime.Format("2006-01-02-15-04-05")
 	out, err := os.Create(realRoomIdStr + "/" + realRoomIdStr + "_" + formattedNowTimeStr + ".flv")
 	if err != nil {
-		log.Fatal(err)
+		log.Default().Printf(err.Error())
+		return
 	}
 	defer out.Close()
 
 	fileResp, err := http.Get(realStreamUrl)
 	if err != nil {
-		log.Fatal(err)
+		log.Default().Printf(err.Error())
+		return
 	}
 
 	defer fileResp.Body.Close()
 
-	fmt.Println("Start downloading...")
+	log.Default().Println("Real Room Id:" + realRoomIdStr + "Start downloading at" + formattedNowTimeStr)
 
 	_, err = io.Copy(out, fileResp.Body)
 
 	if err != nil {
-		log.Fatal(err)
+		log.Default().Printf(err.Error())
+		return
 	}
+}
+
+func main() {
+
+	c := cron.New()
+
+	flag.Parse()
+
+	if *roomId == -1 {
+		fmt.Println("Please input a valid room id.")
+		return
+	}
+
+	c.AddFunc("@every 2s", func() {
+		fmt.Println("hi")
+		roomIdNeedToBeCheck := <-roomChecklist
+		fmt.Println("Now checking:" + strconv.Itoa(roomIdNeedToBeCheck))
+		defer func() { roomChecklist <- roomIdNeedToBeCheck }()
+		realRoomId, status, err := checkLiveStatus(roomIdNeedToBeCheck)
+
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		if !status {
+			fmt.Println("Not living...")
+			return
+		}
+
+		liveVideoDownloader(realRoomId)
+
+	})
+
+	c.Start()
+
+	roomChecklist <- *roomId
+
+	select {}
 
 }
